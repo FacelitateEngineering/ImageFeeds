db4cd552fe85007c30da58b7df1ec345bf04f710
toby <tobyclh@gmail.com>
2023-11-29 18:16:14 +0900

    update image feed

--- setup.py
+++ setup.py
@@ -1,11 +1,10 @@
 from setuptools import setup, find_packages, distutils
 
 setup(name='ImageFeeds',
-      version='0.1',
+      version='0.2',
       description='Python Camera interface',
-      url='http://github.com/tobyclh/ImageFeeds',
-      author='tobyclh',
-      author_email='tobyclh@gmail.com',
+      url='http://github.com/FacelitateEngineering/ImageFeeds',
+      author='FacelitateEngineering',
       license='Copyright',
       packages=find_packages(),
       zip_safe=False,
